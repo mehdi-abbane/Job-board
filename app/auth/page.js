1252d48e8773bbf7a596aa89979397d84ffa45ff
Mehdi <mehdi.work.contact@gmail.com>
2024-12-02 21:22:28 +0100

    Made the ui for the sign up page

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,8 +1,12 @@
 "use client"
 import { useState } from "react"
+import { useForm } from "react-hook-form";
+import Input from "../components/Input";
 
 export default function Auth() {
+
     const [userType, setUserType] = useState(null);
+    const { register, handleSubmit, reset } = useForm()
     return <div className="h-screen w-screen grid place-content-center">
 
         {
@@ -15,15 +19,31 @@ export default function Auth() {
             )
         }
         {
-            userType == "compnay" && <div>
-                company
-                </div>
-        }
-        {
-            userType == "employer" && <div>
-                <input />
+            userType == "company" && <div className="flex flex-col gap-4">
+                <Input register={register} info="name" placeholder="Microfost" label="Name" />
+                <Input register={register} info="pfp" isTextarea={null} label="Profile Picture" />
+                <Input register={register} info="banner" isTextarea={null} label="Banner" />
+                <Input register={register} info="email" type="email" placeholder="Email" label="Company's Email" />
+                <Input register={register} info="description" isTextarea={true} placeholder="Passionate Frontend
+                Developer who bring much value to your technical team" label="Description" />
+                <button className="btn btn-primary">Sign Up</button>
             </div>
         }
-        <p></p>
+        {
+            userType == "employer" && <div className="grid grid-cols-2 gap-2 grid-rows-2 ">
+                <Input register={register} info="pfp" isTextarea={null} label="Profile Picture" />
+                <Input register={register} info="banner" isTextarea={null} label="Banner" />
+                <Input register={register} info="email" type="email" placeholder="Email" label="Email" />
+                <Input register={register} info="name" placeholder="Mehdi Abbane" label="Name" />
+                <Input register={register} info="fullname" placeholder="Mehdi Abbane" label="Full Name" />
+                <Input register={register} info="role" placeholder="Frontend" label="Role" />
+                <Input register={register} info="skills" placeholder="Next.js, react, Javascript" label="Full Name" />
+                <Input register={register} info="description" isTextarea={true} placeholder="Passionate Frontend Developer who bring
+                much value to your technical team" label="Description" />
+                <div className="w-full col-span-2">
+                    <button className="btn btn-primary w-full">Sign Up</button>
+                </div>
+            </div>
+        }
     </div>
 }
--- app/components/Input.js
+++ app/components/Input.js
@@ -1,3 +1,19 @@
-export default function Input({register, info, placeholder="", isFile = false}) {
-
+export default function Input({ register, info, placeholder = "", isTextarea = false, type = "text", label = "" }) {
+    return <div>
+        <div className="label">
+            <span className="label-text">{label}</span>
+        </div>
+        {
+            isTextarea == null &&
+            <input type="file" {...register(info)} className="file-input file-input-bordered w-full max-w-xs" />
+        }
+        {
+            isTextarea == false &&
+            <input {...register(info)} className="input input-bordered w-full max-w-xs" type={type} placeholder={placeholder} />
+        }
+        {
+            isTextarea == true &&
+            <textarea {...register(info)} className="textarea textarea-bordered" placeholder="Bio"></textarea>
+        }
+    </div>
 }
