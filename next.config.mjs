c844f29f5bfa6f70161f016ab0269654681ca4b3
Mehdi <mehdi.work.contact@gmail.com>
2024-12-06 12:19:05 +0100

    Ops

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2YXhzcHZwY21lY2hxenFkZHp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA1NDU3OTksImV4cCI6MjA0NjEyMTc5OX0.7oziTYYdaSRt4DWtZb5xPg3nWuogNvf_Q2FUTyMRAKo",
-        SUPBASE_URL: "https://evaxspvpcmechqzqddzu.supabase.co"
-    }
-};
-
-export default nextConfig;
